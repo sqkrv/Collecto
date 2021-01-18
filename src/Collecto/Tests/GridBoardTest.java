4aa693cf18629255dcda945425829cdfe3021df4
squidass <stasyushka.dobro@list.ru>
2021-01-18 16:10:47 +0100

    GridBoardTest implementation

--- src/Collecto/Tests/GridBoardTest.java
+++ src/Collecto/Tests/GridBoardTest.java
@@ -1,49 +1,318 @@
 package Collecto.Tests;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import Collecto.Ball;
+import Collecto.GridBoard;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GridBoardTest {
+    private static GridBoard board;
+    private static GridBoard sampleBoard;
+    private static GridBoard emptyBoard;
+    private static ArrayList<ArrayList<Ball>> sampleBoardArray;
+    private static final ArrayList<ArrayList<Ball>> emptyBoardArray = new ArrayList<>();
+
+    private ArrayList<ArrayList<Ball>> copyArray(ArrayList<ArrayList<Ball>> array) {
+        ArrayList<ArrayList<Ball>> copy_array = new ArrayList<>();
+
+        for (ArrayList<Ball> row : array) {
+            copy_array.add((ArrayList<Ball>) row.clone());
+        }
+
+        return copy_array;
+    }
+
+    @BeforeAll
+    public static void setSampleBoardArray() {
+        sampleBoardArray = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            sampleBoardArray.add(i, new ArrayList<>());
+        }
+        sampleBoardArray.get(0).addAll(
+                Arrays.asList(
+                        Ball.GREEN,
+                        Ball.YELLOW,
+                        Ball.ORANGE,
+                        Ball.GREEN,
+                        Ball.PURPLE,
+                        Ball.RED,
+                        Ball.ORANGE
+                )
+        );
+        sampleBoardArray.get(1).addAll(
+                Arrays.asList(
+                        Ball.YELLOW,
+                        Ball.BLUE,
+                        Ball.RED,
+                        Ball.ORANGE,
+                        Ball.YELLOW,
+                        Ball.GREEN,
+                        Ball.PURPLE
+                )
+        );
+        sampleBoardArray.get(2).addAll(
+                Arrays.asList(
+                        Ball.BLUE,
+                        Ball.RED,
+                        Ball.PURPLE,
+                        Ball.BLUE,
+                        Ball.RED,
+                        Ball.YELLOW,
+                        Ball.ORANGE
+                )
+        );
+        sampleBoardArray.get(3).addAll(
+                Arrays.asList(
+                        Ball.PURPLE,
+                        Ball.YELLOW,
+                        Ball.GREEN,
+                        Ball.WHITE,
+                        Ball.BLUE,
+                        Ball.RED,
+                        Ball.GREEN
+                )
+        );
+        sampleBoardArray.get(4).addAll(
+                Arrays.asList(
+                        Ball.ORANGE,
+                        Ball.GREEN,
+                        Ball.BLUE,
+                        Ball.GREEN,
+                        Ball.PURPLE,
+                        Ball.BLUE,
+                        Ball.RED
+                )
+        );
+        sampleBoardArray.get(5).addAll(
+                Arrays.asList(
+                        Ball.YELLOW,
+                        Ball.PURPLE,
+                        Ball.ORANGE,
+                        Ball.YELLOW,
+                        Ball.ORANGE,
+                        Ball.YELLOW,
+                        Ball.PURPLE
+                )
+        );
+        sampleBoardArray.get(6).addAll(
+                Arrays.asList(
+                        Ball.ORANGE,
+                        Ball.GREEN,
+                        Ball.BLUE,
+                        Ball.RED,
+                        Ball.BLUE,
+                        Ball.PURPLE,
+                        Ball.RED
+                )
+        );
+    }
+
+    @BeforeAll
+    static void setEmptyBoard() {
+
+        for (int i = 0; i < 7; i++) {
+            emptyBoardArray.add(new ArrayList<>());
+            for (int j = 0; j < 7; j++) {
+                emptyBoardArray.get(i).add(Ball.WHITE);
+            }
+        }
+//        emptyBoardArray.addAll(0, Collections.nCopies(7, Collections.nCopies(7, Ball.WHITE)));
+        emptyBoard = new GridBoard(emptyBoardArray);
+    }
 
     @BeforeEach
     void setUp() {
-    }
-
-    @Test
-    void constructPreset() {
+        board = new GridBoard();
+        sampleBoard = new GridBoard(copyArray(sampleBoardArray));
     }
 
     @Test
     void deepCopy() {
+        GridBoard copy = new GridBoard();
+        assertEquals(board.toString(), copy.toString());
+        // meh
+        copy.moveLine(3, 3, GridBoard.Direction.UP);
+        assertNotEquals(board.toString(), copy.toString());
     }
 
-    @Test
-    void moveLine() {
+    @Nested
+    @DisplayName("Tests for method moveLine")
+    class moveLine {
+
+        @Nested
+        @DisplayName("Tests for rows and columns with blank spaces")
+        class moveLineWithBlank {
+
+            @Test
+            @DisplayName("moveLine UP")
+            void moveLineWBUP() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(3, 3, GridBoard.Direction.UP);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(3).set(3, Ball.GREEN);
+                copy.get(4).set(3, Ball.YELLOW);
+                copy.get(5).set(3, Ball.RED);
+                copy.get(6).set(3, Ball.WHITE);
+                assertEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+            }
+
+            @Test
+            @DisplayName("moveLine DOWN")
+            void moveLineWBDOWN() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(3, 3, GridBoard.Direction.DOWN);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(0).set(3, Ball.WHITE);
+                copy.get(1).set(3, Ball.GREEN);
+                copy.get(2).set(3, Ball.ORANGE);
+                copy.get(3).set(3, Ball.BLUE);
+                assertEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+            }
+
+            @Test
+            @DisplayName("moveLine LEFT")
+            void moveLineWBLEFT() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(3, 3, GridBoard.Direction.LEFT);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(3).set(3, Ball.BLUE);
+                copy.get(3).set(4, Ball.RED);
+                copy.get(3).set(5, Ball.GREEN);
+                copy.get(3).set(6, Ball.WHITE);
+                assertEquals(sampleBoard.toString(), new GridBoard(copy).toString());
+            }
+
+            @Test
+            @DisplayName("moveLine RIGHT")
+            void moveLineWBRIGHT() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(3, 3, GridBoard.Direction.RIGHT);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(3).set(0, Ball.WHITE);
+                copy.get(3).set(1, Ball.PURPLE);
+                copy.get(3).set(2, Ball.YELLOW);
+                copy.get(3).set(3, Ball.GREEN);
+                assertEquals(sampleBoard.toString(), new GridBoard(copy).toString());
+            }
+        }
+
+        @Nested
+        @DisplayName("Tests for rows and columns without spaces")
+        class moveLineWithoutBlank {
+            @Test
+            @DisplayName("moveLine (without WHITE) UP")
+            void moveLineWOBUP() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(6, 6, GridBoard.Direction.UP);
+                assertEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(6).set(6, Ball.WHITE);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+            }
+
+            @Test
+            @DisplayName("moveLine (without WHITE) DOWN")
+            void moveLineWOBDOWN() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(6, 6, GridBoard.Direction.DOWN);
+                assertEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(6).set(6, Ball.WHITE);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+            }
+
+            @Test
+            @DisplayName("moveLine (without WHITE) LEFT")
+            void moveLineWOBLEFT() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(6, 6, GridBoard.Direction.LEFT);
+                assertEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(6).set(6, Ball.WHITE);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+            }
+
+            @Test
+            @DisplayName("moveLine (without WHITE) RIGHT")
+            void moveLineWOBRIGHT() {
+                ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+                sampleBoard.moveLine(6, 6, GridBoard.Direction.RIGHT);
+                assertEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+                copy.get(6).set(6, Ball.WHITE);
+                assertNotEquals(sampleBoard.toString(), (new GridBoard(copy)).toString());
+            }
+        }
     }
 
-    @Test
-    void getField() {
+    @Nested
+    @DisplayName("isMoveValid tests")
+    class isMoveValidTests {
+        @Test
+        @DisplayName("Moves at (3,3)")
+        void isMoveValidMiddle() {
+            assertTrue(sampleBoard.isMoveValid(3, 3, GridBoard.Direction.UP));
+            assertTrue(sampleBoard.isMoveValid(3, 3, GridBoard.Direction.DOWN));
+            assertTrue(sampleBoard.isMoveValid(3, 3, GridBoard.Direction.RIGHT));
+            assertTrue(sampleBoard.isMoveValid(3, 3, GridBoard.Direction.LEFT));
+        }
+
+        @Test
+        @DisplayName("Moves at (0,0)")
+        void isMoveValid() {
+            assertFalse(sampleBoard.isMoveValid(0, 0, GridBoard.Direction.LEFT));
+            assertFalse(sampleBoard.isMoveValid(0, 0, GridBoard.Direction.RIGHT));
+            assertFalse(sampleBoard.isMoveValid(0, 0, GridBoard.Direction.UP));
+            assertFalse(sampleBoard.isMoveValid(0, 0, GridBoard.Direction.DOWN));
+        }
+        @Test
+        @DisplayName("Moves at (0,0)")
+        void isMoveValidMiddleInvalid() {
+            ArrayList<ArrayList<Ball>> copy = copyArray(sampleBoardArray);
+            assertTrue((new GridBoard(copy)).isMoveValid(3,3,GridBoard.Direction.UP));
+            copy.get(4).set(3, Ball.RED);
+            assertFalse((new GridBoard(copy)).isMoveValid(3,3,GridBoard.Direction.UP));
+        }
     }
 
-    @Test
-    void setField() {
-    }
+//    @Test
+//    void getField() {
+//    }
 
-    @Test
-    void validIndex() {
-    }
+//    @Test
+//    void setField() {
+//    }
 
-    @Test
-    void getCoordinates() {
-    }
+//    @Test
+//    void validIndex() {
+//    }
+//
+//    @Test
+//    void getCoordinates() {
+//
+//    }
 
-    @Test
-    void isMoveValid() {
-    }
+    @Nested
+    class possibleMove {
+        @Test
+        void possibleMovesSample() {
+            assertTrue(sampleBoard.possibleMoves());
+            sampleBoardArray.get(3).set(3, Ball.RED);
+            assertFalse((new GridBoard(sampleBoardArray)).possibleMoves());
+        }
 
-    @Test
-    void possibleMoves() {
+        @Test
+        void possibleMovesEmpty() {
+            assertFalse(emptyBoard.possibleMoves());
+            emptyBoardArray.get(0).set(0, Ball.GREEN);
+            emptyBoardArray.get(0).set(6, Ball.GREEN);
+            assertTrue((new GridBoard(emptyBoardArray)).possibleMoves());
+        }
+
+        @Test
+        void testTwoMoves() {
+            emptyBoardArray.get(0).set(0, Ball.GREEN);
+            emptyBoardArray.get(6).set(6, Ball.GREEN);
+            assertTrue((new GridBoard(emptyBoardArray)).possibleMoves()); //TODO: implement functionality to GridBoard
+        }
     }
 }
