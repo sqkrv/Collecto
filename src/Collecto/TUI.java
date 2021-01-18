91b1ab180a16108e33dce00a4bb39b014e2b64bf
squidass <stasyushka.dobro@list.ru>
2021-01-18 15:11:22 +0100

    Moved toString "method" to GridBoardTest (and temporarily to GridBoard)

--- src/Collecto/TUI.java
+++ src/Collecto/TUI.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TUI {
+
+
     public static void main(String[] args) {
         GridBoard board = new GridBoard();
         GridBoard copy = board.deepCopy();
@@ -14,31 +16,8 @@ public class TUI {
 //        System.out.println(board.checkSurroundings(0, 0));
         System.out.println(board.possibleMoves());
 
-        System.out.println();
-        System.out.print("     |");
-        for (int i = 1; i <= 7; i++) {
-            System.out.print("   "+i+"   |");
-        }
+        System.out.println(board.toString());
 
-        System.out.println();
-
-        for (int i = 1; i <= 7; i++) {
-            System.out.print("-----+");
-            for (int j = 1; j <= 7; j++) {
-                System.out.print("-------+");
-            }
-            System.out.println();
-            System.out.print("  "+i+"  |");
-            for (int j = 1; j <= 7; j++) {
-                System.out.printf("%-16s|", board.getField(i-1, j-1));
-            }
-            System.out.println();
-        }
-        System.out.print("-----+");
-        for (int j = 1; j <= 7; j++) {
-            System.out.print("-------+");
-        }
-
-        System.out.println("  Is board the same: "+board.board.equals(copy.board));
+        System.out.println("Is board the same: "+board.toString().equals(copy.toString()));
     }
 }
