20160d98610877053cf65311d83f255a289c4ecb
squidass <stasyushka.dobro@list.ru>
2021-01-20 00:36:39 +0100

    TUI changes moved all TUI things to TUI class

--- src/Collecto/TUI.java
+++ src/Collecto/TUI.java
@@ -1,11 +1,59 @@
 package Collecto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TUI {
+    private static String grey(String string) {
+        return "\u001B[37m" + string + "\u001B[0m";
+    }
+
+    public static String playersBoard(Player player1, Player player2) {
+        StringBuilder string = new StringBuilder();
+        HashMap<Ball, Integer> p1Balls = player1.showBalls();
+        HashMap<Ball, Integer> p2Balls = player2.showBalls();
+
+        int max_length = player1.getName().length();
+        if (player2.getName().length() > max_length) max_length = player2.getName().length();
+
+        string.append(" ".repeat(max_length+1)).append(grey("|"));
+        for (Map.Entry<Ball, Integer> pair : p1Balls.entrySet()) {
+            string.append(String.format("%-16s"+grey("|"), pair.getKey().toColouredString()));
+        }
+        string.append("Points ").append(grey("|"));
+
+        string.append(player_row(player1, p1Balls, max_length));
+
+        string.append(player_row(player2, p2Balls, max_length));
+
+        string.append("\n");
+        string.append(grey("-".repeat(max_length+1)+"+"));
+        string.append(grey("-------+").repeat(7));
+
+        return string.toString();
+    }
+
+    private static String player_row(Player player, HashMap<Ball, Integer> balls, int max_length) {
+        StringBuilder string = new StringBuilder();
+
+        string.append("\n");
+        string.append(grey("-".repeat(max_length+1)+"+"));
+        string.append(grey("-------+".repeat(7)));
+        string.append("\n");
+        string.append(String.format("%"+(max_length+1)+"s"+grey("|"), player.getName()));
+        for (Map.Entry<Ball, Integer> pair : balls.entrySet()) {
+            string.append(String.format("%-7s"+grey("|"), pair.getValue()));
+        }
+        string.append(String.format("%-7s", player.getPoints())).append(grey("|"));
+
+        return string.toString();
+    }
+
     public static String plainTextBoard(GridBoard gridBoard) {
         return plainTextBoard(gridBoard, false);
     }
 
-    public static String plainTextBoard(GridBoard gridBoard, boolean coloured) {
+    private static String plainTextBoard(GridBoard gridBoard, boolean coloured) {
         StringBuilder string = new StringBuilder();
 
         string.append("     |");
