3d528be7fb484505a52f7b927c063044edbf2681
squidass <stasyushka.dobro@list.ru>
2021-01-20 00:37:37 +0100

    some useless stuff

--- src/Collecto/Main.java
+++ src/Collecto/Main.java
@@ -1,11 +1,25 @@
 package Collecto;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        if (args.length != 2) System.out.println("die");
+
         Player player1;
         Player player2;
-        if (args.length != 2) System.out.println("die");
+        String name;
+        Scanner input = new Scanner(System.in);
+
         System.out.println("p1 name");
-//        String name = ;
+        name = input.nextLine();
+//        player1 = new HumanPlayer(name);
+
+        System.out.println("p2 name");
+        name = input.nextLine();
+//        player2 = new HumanPlayer(name);
+
+//        Game game = new Game(player1, player2);
+//        game.start();
     }
 }
