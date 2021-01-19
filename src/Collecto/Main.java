abcbfe902fca7537fb761214ea9a83a201a50593
squidass <stasyushka.dobro@list.ru>
2021-01-19 17:19:45 +0100

    new dummy Main class for psvm

---
+++ src/Collecto/Main.java
@@ -0,0 +1,11 @@
+package Collecto;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player1;
+        Player player2;
+        if (args.length != 2) System.out.println("die");
+        System.out.println("p1 name");
+//        String name = ;
+    }
+}
