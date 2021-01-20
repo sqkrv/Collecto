478a7a2cc9584a005650b4d185884600b7e40aa4
squidass <stasyushka.dobro@list.ru>
2021-01-20 14:48:01 +0100

    the great return of Misc class

---
+++ src/Collecto/Misc.java
@@ -0,0 +1,20 @@
+package Collecto;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Misc {
+    static DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd/MM");
+
+    public static String logTime() {
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(logTime());
+    }
+}
