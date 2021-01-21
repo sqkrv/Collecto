6ba550391c7c51a26b06f4263cd71182010d9a9f
squidass <stasyushka.dobro@list.ru>
2021-01-21 23:03:31 +0100

    Nice edits and new Move class

--- src/Collecto/Misc.java
+++ src/Collecto/Misc.java
@@ -1,7 +1,5 @@
 package Collecto;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,7 +8,13 @@ public class Misc {
     static DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd.MM");
 
     public static String logTime() {
-        return "["+currentDateTime()+"] - ";
+        return logTime(true);
+    }
+
+    public static String logTime(boolean dash) {
+        String string = "["+currentDateTime()+"] ";
+        if (dash) return string+"— ";
+        return string;
     }
 
     public static String currentDateTime() {
@@ -20,5 +24,24 @@ public class Misc {
 
     public static void main(String[] args) {
         System.out.println(logTime());
+    }
+
+    public static class Move {
+        private final GridBoard.Direction direction;
+        private final int line;
+
+        public Move(int line, GridBoard.Direction direction) {
+            assert 0 <= line && line < 7;
+            this.direction = direction;
+            this.line = line;
+        }
+
+        public GridBoard.Direction getDirection() {
+            return direction;
+        }
+
+        public int getLine() {
+            return line;
+        }
     }
 }
