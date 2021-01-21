a3b892daf023208851bd92bf0fab56ba5d30b73c
squidass <stasyushka.dobro@list.ru>
2021-01-21 23:02:47 +0100

    New Controller thingy

---
+++ src/Collecto/Controller.java
@@ -0,0 +1,56 @@
+package Collecto;
+
+import java.util.Scanner;
+
+public class Controller {
+    private final Scanner scanner;
+    private final Client client;
+
+    public final static String COMMANDS = "LOGIN, LIST, QUEUE, MOVE";
+
+    public Controller(Client client) {
+        this.client = client;
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        String input;
+        while ((input = scanner.nextLine()) != null) {
+            handleCommand(input);
+        }
+    }
+
+    private void handleCommand(String input) {
+        if (input == null) {
+            return;
+        }
+        String[] params = input.split(" ");
+        switch (params[0]) {
+            case "LIST":
+            case "QUEUE":
+                client.sendMessage(params[0]);
+                break;
+            case "MOVE":
+                handleMove(params);
+                break;
+            default:
+                TUI.printError("Unknown command: " + params[0]);
+                TUI.print("Instead use: " + COMMANDS);
+        }
+    }
+
+    private void handleMove(String[] params) {
+        if (params.length <= 1) {
+            TUI.print("Insufficient arguments, try again");
+        } else {
+            String message = params[0] + Server.DELIMITER + params[1];
+            if (params.length >= 3) message += Server.DELIMITER + params[2];
+            client.sendMessage(message);
+        }
+    }
+
+    protected String handleLogin() {
+        return scanner.nextLine();
+    }
+
+}
