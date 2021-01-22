d49d752b89ba1fc99bf0a5616e771a302d2f8c89
squidass <stasyushka.dobro@list.ru>
2021-01-22 16:01:21 +0100

    moved out common method copyArray to Tests.Misc

--- src/Collecto/Tests/GridBoardTest.java
+++ src/Collecto/Tests/GridBoardTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.*;
 import java.util.*;
 
 import static Collecto.Misc.Move;
+import static Collecto.Tests.Misc.copyArray;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,99 +19,9 @@ class GridBoardTest {
     private static ArrayList<ArrayList<Ball>> sampleBoardArray;
     private static final ArrayList<ArrayList<Ball>> emptyBoardArray = new ArrayList<>();
 
-    private ArrayList<ArrayList<Ball>> copyArray(ArrayList<ArrayList<Ball>> array) {
-        ArrayList<ArrayList<Ball>> copy_array = new ArrayList<>();
-
-        for (ArrayList<Ball> row : array) {
-            copy_array.add((ArrayList<Ball>) row.clone());
-        }
-
-        return copy_array;
-    }
-
     @BeforeAll
-    public static void setSampleBoardArray() {
-        sampleBoardArray = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
-            sampleBoardArray.add(i, new ArrayList<>());
-        }
-        sampleBoardArray.get(0).addAll(
-                Arrays.asList(
-                        Ball.GREEN,
-                        Ball.YELLOW,
-                        Ball.ORANGE,
-                        Ball.GREEN,
-                        Ball.PURPLE,
-                        Ball.RED,
-                        Ball.ORANGE
-                )
-        );
-        sampleBoardArray.get(1).addAll(
-                Arrays.asList(
-                        Ball.YELLOW,
-                        Ball.BLUE,
-                        Ball.RED,
-                        Ball.ORANGE,
-                        Ball.YELLOW,
-                        Ball.GREEN,
-                        Ball.PURPLE
-                )
-        );
-        sampleBoardArray.get(2).addAll(
-                Arrays.asList(
-                        Ball.BLUE,
-                        Ball.RED,
-                        Ball.PURPLE,
-                        Ball.BLUE,
-                        Ball.RED,
-                        Ball.YELLOW,
-                        Ball.ORANGE
-                )
-        );
-        sampleBoardArray.get(3).addAll(
-                Arrays.asList(
-                        Ball.PURPLE,
-                        Ball.YELLOW,
-                        Ball.GREEN,
-                        Ball.WHITE,
-                        Ball.BLUE,
-                        Ball.RED,
-                        Ball.GREEN
-                )
-        );
-        sampleBoardArray.get(4).addAll(
-                Arrays.asList(
-                        Ball.ORANGE,
-                        Ball.GREEN,
-                        Ball.BLUE,
-                        Ball.GREEN,
-                        Ball.PURPLE,
-                        Ball.BLUE,
-                        Ball.RED
-                )
-        );
-        sampleBoardArray.get(5).addAll(
-                Arrays.asList(
-                        Ball.YELLOW,
-                        Ball.PURPLE,
-                        Ball.ORANGE,
-                        Ball.YELLOW,
-                        Ball.ORANGE,
-                        Ball.YELLOW,
-                        Ball.PURPLE
-                )
-        );
-        sampleBoardArray.get(6).addAll(
-                Arrays.asList(
-                        Ball.ORANGE,
-                        Ball.GREEN,
-                        Ball.BLUE,
-                        Ball.RED,
-                        Ball.BLUE,
-                        Ball.PURPLE,
-                        Ball.RED
-                )
-        );
+    static void setSampleBoardArray() {
+        sampleBoardArray = Misc.sampleBoardArray();
     }
 
     @BeforeAll
