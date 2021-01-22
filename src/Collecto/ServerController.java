5ce41b8fc5f99a8c56022ae8d56643ad830eedd3
squidass <stasyushka.dobro@list.ru>
2021-01-22 21:31:55 +0100

    i said that idk why is it still here

--- src/Collecto/ServerController.java
+++ src/Collecto/ServerController.java
@@ -1,13 +1,4 @@
 package Collecto;
 
-import java.util.Scanner;
-
 public class ServerController extends Controller {
-//    public ServerController() {
-////        this.server = server;
-//    }
-
-    protected String promptUser() {
-        return scanner.nextLine().trim();
-    }
 }
