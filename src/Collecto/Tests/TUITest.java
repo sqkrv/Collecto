6e401ad8465d68970b875e2c04073634a6b5f418
squidass <stasyushka.dobro@list.ru>
2021-01-23 23:56:35 +0100

    And even a separate test!

---
+++ src/Collecto/Tests/TUITest.java
@@ -0,0 +1,76 @@
+package Collecto.Tests;
+
+import Collecto.GridBoard;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TUITest {
+
+
+//    public static void main(String[] args) {
+//        GridBoard board = new GridBoard();
+//        GridBoard copy = board.deepCopy();
+//
+////        board.moveLine(4, 4, GridBoard.Direction.DOWN);
+////        System.out.println(board.isMoveValid(4,4, GridBoard.Direction.DOWN));
+//
+////        System.out.println(board.checkSurroundings(0, 0));
+////        System.out.println(board.possibleMoves());
+//
+//        System.out.println(toString(board));
+//        System.out.println(plainTextBoard(board));
+//        System.out.println(colouredBoard(board));
+//
+//        System.out.println("Is board the same: "+board.toString().equals(copy.toString()));
+//    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void logTime() {
+    }
+
+    @Test
+    void currentDateTime() {
+    }
+
+    @Test
+    void plainTextBoard() {
+    }
+
+    @Test
+    void colouredBoard() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void printError() {
+    }
+
+    @Test
+    void log() {
+    }
+
+    @Test
+    void printHelpClient() {
+    }
+
+    @Test
+    void ballColours() {
+    }
+
+    @Test
+    void main() {
+    }
+}
