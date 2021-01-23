24ec174f22cccfc6e8fc8ad98cd7e53e9d2e6acb
squidass <stasyushka.dobro@list.ru>
2021-01-23 15:32:05 +0100

    new temp class for common methods

---
+++ src/Collecto/Temp.java
@@ -0,0 +1,19 @@
+package Collecto;
+
+import static Collecto.Misc.parseInt;
+
+public class Temp {
+    public static boolean isPushValid(int push) {
+        return 0 <= push && push <= 27;
+    }
+
+    public static Integer parsePush(String string_push) {
+        Integer push = parseInt(string_push);
+        if (push != null) {
+            if (isPushValid(push)) {
+                return push;
+            }
+        }
+        return null;
+    }
+}
