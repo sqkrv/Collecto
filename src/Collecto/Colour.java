b552372534e60a8f38d56d517976df54baa4df8e
squidass <stasyushka.dobro@list.ru>
2021-01-24 14:27:16 +0100

    nice new class for fancy Colours

---
+++ src/Collecto/Colour.java
@@ -0,0 +1,52 @@
+package Collecto;
+
+public class Colour {
+    public static final String RESET = "\u001B[0m";
+
+    public static final String BLACK = "\u001B[30m";
+    public static final String GREY = "\u001B[37m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[93m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+    public static final String ORANGE = "\u001B[33m";
+
+    public static String black(String string) {
+        return BLACK + string + RESET;
+    }
+
+    public static String grey(String string) {
+        return GREY + string + RESET;
+    }
+
+    public static String red(String string) {
+        return RED + string + RESET;
+    }
+
+    public static String green(String string) {
+        return GREEN + string + RESET;
+    }
+
+    public static String yellow(String string) {
+        return YELLOW + string + RESET;
+    }
+
+    public static String blue(String string) {
+        return BLUE + string + RESET;
+    }
+
+    public static String purple(String string) {
+        return PURPLE + string + RESET;
+    }
+
+    public static String cyan(String string) {
+        return CYAN + string + RESET;
+    }
+
+    public static String orange(String string) {
+        return ORANGE + string + RESET;
+    }
+}
