9a7723faf7df404a141d64d36717baec8fd60d6b
squidass <stasyushka.dobro@list.ru>
2021-01-24 14:33:40 +0100

    Balls, say hi to Colour

--- src/Collecto/Ball.java
+++ src/Collecto/Ball.java
@@ -1,9 +1,9 @@
 package Collecto;
 
+import static Collecto.Colour.*;
+
 public enum Ball {
     WHITE, BLUE, YELLOW, RED, ORANGE, PURPLE, GREEN;
-
-    private static final String RESET_COLOUR = "\u001B[0m";
 
     public String getColour() {
         return this.name();
@@ -17,35 +17,35 @@ public enum Ball {
 
     public String toColouredString() {
         if (this == BLUE) {
-            return "\u001B[34m" + "BLUE" + RESET_COLOUR;
+            return blue(this.toString());
         } else if (this == YELLOW) {
-            return "\u001B[93m" + "YELLOW" + RESET_COLOUR;
+            return yellow(this.toString());
         } else if (this == RED) {
-            return "\u001B[31m" + "RED" + RESET_COLOUR;
+            return red(this.toString());
         } else if (this == ORANGE) {
-            return "\u001B[33m" + "ORANGE" + RESET_COLOUR;
+            return orange(this.toString());
         } else if (this == PURPLE) {
-            return "\u001B[35m" + "PURPLE" + RESET_COLOUR;
+            return purple(this.toString());
         } else if (this == GREEN) {
-            return "\u001B[32m" + "GREEN" + RESET_COLOUR;
+            return green(this.toString());
         } else {
-            return "\u001B[37m" + "" + RESET_COLOUR;
+            return Colour.WHITE + this.toString() + RESET;
         }
     }
 
     public String ballColour() {
         if (this == BLUE) {
-            return "\u001B[34m";
+            return Colour.BLUE;
         } else if (this == YELLOW) {
-            return "\u001B[93m";
+            return Colour.YELLOW;
         } else if (this == RED) {
-            return "\u001B[31m";
+            return Colour.RED;
         } else if (this == ORANGE) {
-            return "\u001B[33m";
+            return Colour.ORANGE;
         } else if (this == PURPLE) {
-            return "\u001B[35m";
+            return Colour.PURPLE;
         } else if (this == GREEN) {
-            return "\u001B[32m";
+            return Colour.GREEN;
         } else {
             return RESET_COLOUR;
         }
