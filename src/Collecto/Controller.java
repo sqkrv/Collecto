daa1452d53d160133fb8d9da7a507f9cd08e5986
squidass <stasyushka.dobro@list.ru>
2021-01-24 14:32:21 +0100

    javadoc and little checkIP/Port fixes

--- src/Collecto/Controller.java
+++ src/Collecto/Controller.java
@@ -6,39 +6,61 @@ import java.util.Scanner;
 public class Controller {
     protected final Scanner scanner;
 
+    /**
+     * Constructs a Controller
+     */
     public Controller() {
         this.scanner = new Scanner(System.in);
     }
 
+    /**
+     * Prints provided message and then returns line input by the user after the message containing no leading and trailing spaces
+     * @param message message to output before prompting the user
+     * @ensures line contains no leading and trailing spaces
+     * @return input line
+     */
     protected synchronized String promptUser(String message) {
         TUI.print(message);
         return promptUser();
     }
 
+    /**
+     * Returns line input by the user containing no leading and trailing spaces
+     * @ensures line contains no leading and trailing spaces
+     * @return input line
+     */
     protected synchronized String promptUser() {
         return scanner.nextLine().trim();
     }
 
-    protected InetAddress promptIP() {
+    /**
+     * Prompts user for an host address until it verifies validness of this address. Then returns this address.
+     * @ensures host address is valid InetAddress
+     * @return InetAddress to use in Sockets
+     */
+    protected InetAddress promptAddress() {
         String prompt;
         InetAddress ip;
         while (true) {
-            TUI.print("Please enter IP address:");
-            prompt = promptUser();
-            ip = Misc.checkIP(prompt);
+            prompt = promptUser("Please enter host address:");
+            ip = Global.checkAddress(prompt);
             if (ip != null) break;
-            TUI.print("Wrong IP provided");
+            TUI.print("Wrong address provided");
         }
         return ip;
     }
 
+    /**
+     * Prompts user for a port until it verifies validness of this port. Then returns this port.
+     * @ensures port is valid positive integer
+     * @return port
+     */
     protected Integer promptPort() {
         String prompt;
         Integer port;
         while (true) {
-            TUI.print("Please enter port of the server:");
-            prompt = promptUser();
-            port = Misc.checkPort(prompt);
+            prompt = promptUser("Please enter port of the server:");
+            port = Global.checkPort(prompt);
             if (port != null) break;
             TUI.print("Wrong port provided");
         }
