e6ed58cf0dd77be0a6909485066cd27c70dad1d9
squidass <stasyushka.dobro@list.ru>
2021-01-25 17:39:46 +0100

    i guess pretty done PlayerTest

--- src/Collecto/Tests/PlayerTest.java
+++ src/Collecto/Tests/PlayerTest.java
@@ -5,10 +5,7 @@ import Collecto.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,16 +19,41 @@ class PlayerTest {
     }
 
     @Test
-    void makeMove() {
-        //TODO: implement this method
+    void addBalls() {
+        ArrayList<Ball> balls = new ArrayList<>();
+        HashMap<Ball, Integer> resultBalls = new HashMap<>();
+
+        balls.add(Ball.BLUE);
+        for (Ball ball : Ball.values()) {
+            if (ball != Ball.WHITE) {
+                resultBalls.put(ball, 0);
+            }
+        }
+        resultBalls.put(Ball.BLUE, 1);
+
+        assertNotEquals(player.getBalls(), resultBalls);
+
+        player.addBalls(balls);
+        assertEquals(player.getBalls(), resultBalls);
+
+        balls.addAll(Arrays.asList(Ball.BLUE, Ball.BLUE, Ball.YELLOW));
+
+        player.addBalls(balls);
+        resultBalls.put(Ball.BLUE, 4);
+        resultBalls.put(Ball.YELLOW, 1);
+
+        assertEquals(player.getBalls(), resultBalls);
     }
 
     @Test
-    void addBalls() {
+    void addWhiteBall() {
         ArrayList<Ball> balls = new ArrayList<>();
-//        balls.addAll(Ball.BLUE);
-//        assertNotEquals(player, new Player());
-        //TODO: check if this can be tested somehow
+        HashMap<Ball, Integer> resultBalls = new HashMap<>();
+        for (Ball ball : Ball.values()) if (ball != Ball.WHITE) resultBalls.put(ball, 0);
+
+        balls.add(Ball.WHITE);
+        player.addBalls(balls);
+        assertEquals(player.getBalls(), resultBalls);
     }
 
     @Test
@@ -45,5 +67,35 @@ class PlayerTest {
         // 11*2*blue balls = 22/3 points = 7 points;
         // 10*yellow balls = 10/3 points = 3 points;
         assertEquals(player.getPoints(), 10);
+    }
+
+    @Test
+    void getName() {
+        assertEquals(player.getName(), "test");
+        String name = "second test player";
+        Player player1 = new Player(name);
+        assertEquals(player1.getName(), name);
+    }
+
+    @Test
+    void getBallAmount() {
+        assertEquals(player.getBallAmount(), 0);
+
+        ArrayList<Ball> balls = new ArrayList<>();
+        balls.add(Ball.PURPLE);
+
+        player.addBalls(balls);
+        assertEquals(player.getBallAmount(), 1);
+
+        balls.addAll(Arrays.asList(Ball.BLUE, Ball.BLUE, Ball.RED));
+        player.addBalls(balls);
+
+        assertEquals(player.getBallAmount(), 5);
+
+        ArrayList<Ball> balls2 = new ArrayList<>();
+        balls2.add(Ball.WHITE);
+        player.addBalls(balls2);
+
+        assertEquals(player.getBallAmount(), 5);
     }
 }
