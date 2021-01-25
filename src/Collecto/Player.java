3d76a6237d62b1ced3025e729f74e7f03f89d4ac
squidass <stasyushka.dobro@list.ru>
2021-01-25 17:36:47 +0100

    probably fully done Player class with all javadocs and stuff cs checked

--- src/Collecto/Player.java
+++ src/Collecto/Player.java
@@ -12,36 +12,46 @@ import java.util.HashMap;
  * @see Ball
  */
 public class Player {
+    /**
+     * HashMap which stores balls of all colours, except for WHITE,
+     * and their amounts.
+     */
     private final HashMap<Ball, Integer> balls = new HashMap<>();
-
     private final String name;
 
     /**
      * Constructs a Player instance with the specified name.
+     *
      * @param name the name of the player
      */
     public Player(String name) {
         this.name = name;
 
         for (Ball ball : Ball.values()) {
-            if (ball != Ball.WHITE) balls.put(ball, 0);
+            if (ball != Ball.WHITE) {
+                balls.put(ball, 0);
+            }
         }
     }
 
     /**
-     * Adds all balls in a specified array to this Player.
+     * Adds all ball colours except for WHITE in a specified array to balls of this Player.
+     *
+     * @param ballsArray an array of balls
      * @ensures no WHITE balls will be added
-     * @param balls an array of balls
      */
-    public void addBalls(ArrayList<Ball> balls) {
-        for (Ball ball : balls) {
-            if (ball != Ball.WHITE) this.balls.put(ball, this.balls.get(ball) + 1);
+    public void addBalls(ArrayList<Ball> ballsArray) {
+        for (Ball ball : ballsArray) {
+            if (ball != Ball.WHITE) {
+                this.balls.put(ball, this.balls.get(ball) + 1);
+            }
         }
     }
 
     /**
      * Returns calculated total point for this Player, where 3 points are awarded for every
-     * 3 balls of the same colour which this Player has acquired.
+     * 3 balls of the same colour that this Player has acquired.
+     *
      * @return total points for this player
      */
     public int getPoints() {
@@ -53,8 +63,9 @@ public class Player {
     }
 
     /**
-     * Returns a HashMap of the balls of this player containing the balls and the corresponding amount that
-     * this player has acquired of that colour ball.
+     * Returns a HashMap of the different colour balls this player has obtained
+     * and the corresponding amount of that colour ball that this player has acquired.
+     *
      * @return player's balls
      */
     public HashMap<Ball, Integer> getBalls() {
@@ -63,6 +74,7 @@ public class Player {
 
     /**
      * Returns the total amount of balls that this player currently has.
+     *
      * @return amount of balls
      */
     public int getBallAmount() {
