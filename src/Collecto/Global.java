a0d1d835bdb57efdd864a3adc1fac5aecbaf29df
squidass <stasyushka.dobro@list.ru>
2021-01-25 20:39:56 +0100

    probably fully done Global class with all javadocs and stuff cs checked

--- src/Collecto/Global.java
+++ src/Collecto/Global.java
@@ -20,8 +20,9 @@ public class Global {
 
     /**
      * Determines whether a move is within the limits of the board.
+     *
      * @param push move in push form
-     * @return true if push >= 0 && push <= 27
+     * @return true if {@code 0 <= push <= 27}
      */
     public static boolean isPushValid(int push) {
         return push >= 0 && push <= 27;
@@ -29,11 +30,12 @@ public class Global {
 
     /**
      * Parses the String of a move in push form to an integer, and checks if that push is valid.
-     * @param string_push push in String form
+     *
+     * @param stringPush push in String form
      * @return integer of push if it is valid, or null
      */
-    public static Integer parsePush(String string_push) {
-        Integer push = parseInt(string_push);
+    public static Integer parsePush(String stringPush) {
+        Integer push = parseInt(stringPush);
         if (push != null) {
             if (isPushValid(push)) {
                 return push;
@@ -43,7 +45,8 @@ public class Global {
     }
 
     /**
-     * Checks whether a given String IP is correct and returns it in InetAddress form.
+     * Checks whether a given String IP is correct and returns it as an InetAddress.
+     *
      * @param ip IP address given in String form
      * @return InetAddress of IP if it is valid, null if it is not
      */
@@ -57,14 +60,17 @@ public class Global {
 
     /**
      * Checks to see if the String of a port is correct and in use, and returns it as an Integer.
-     * @param string_port String of the port
-     * @return Integer if the port is usable, or null
+     *
+     * @param stringPort String of the port
+     * @return Integer if the port is usable, otherwise null
      */
-    public static Integer checkPort(String string_port) {
+    public static Integer checkPort(String stringPort) {
         int port;
         try {
-            port = Integer.parseInt(string_port);
-            if (port > 0) return port;
+            port = Integer.parseInt(stringPort);
+            if (port > 0) {
+                return port;
+            }
             return null;
         } catch (NumberFormatException ignored) {
         }
@@ -73,6 +79,7 @@ public class Global {
 
     /**
      * Parses a String to an Integer.
+     *
      * @param string given String
      * @return Integer if the String can be parsed, or null
      */
@@ -85,12 +92,20 @@ public class Global {
     }
 
     /**
-     * This class contains classes holding strings that are used often in the whole of the Collecto program
-     * as well as the TYPE_HELP string containing a statement referring to the help menu in clients.
+     * This class contains classes holding strings that are used often
+     * in the entirety of the Collecto program, as well as the TYPE_HELP string
+     * which contains a statement referring to the help menu in clients.
+     * @see Commands
+     * @see Misc
+     * @see Extensions
+     * @see Win
      */
     static class Protocol {
+        public static final String TYPE_HELP = "Type HELP to see list of all commands";
+
         /**
-         * This class contains simple commands to be used by clients to check for input from their users.
+         * This class contains simple commands to be used by clients
+         * to check for input from their users.
          */
         static class Commands {
             public static final String LIST = "LIST";
@@ -102,9 +117,10 @@ public class Global {
             public static final String DISCONNECT = "DISCONNECT";
             public static final String BOARD = "BOARD";
         }
+
         /**
-         * This class contains some miscellaneous commands to be used by clients and servers to compare to
-         * outside input.
+         * This class contains some miscellaneous commands to be used by clients
+         * and servers to compare to outside input.
          */
         static class Misc {
             public static final String HELLO = "HELLO";
@@ -114,6 +130,7 @@ public class Global {
             public static final String GAMEOVER = "GAMEOVER";
             public static final String ERROR = "ERROR";
         }
+
         /**
          * This class contains strings for the possible extensions that a server or client can have.
          */
@@ -123,6 +140,7 @@ public class Global {
             public static final String AUTH = "AUTH";
             public static final String CRYPT = "CRYPT";
         }
+
         /**
          * This class contains strings of reasons for winning a game.
          */
@@ -131,7 +149,5 @@ public class Global {
             public static final String VICTORY = "VICTORY";
             public static final String DISCONNECT = "DISCONNECT";
         }
-
-        public static final String TYPE_HELP = "Type HELP to see list of all commands";
     }
 }
