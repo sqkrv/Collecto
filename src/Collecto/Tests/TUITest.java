e318fa30ac856b948138f9752b8981c8af8a7048
squidass <stasyushka.dobro@list.ru>
2021-01-26 01:40:18 +0100

    nicely done TUITest which fully covers TUI class cs checked

--- src/Collecto/Tests/TUITest.java
+++ src/Collecto/Tests/TUITest.java
@@ -1,72 +1,132 @@
 package Collecto.Tests;
 
-import Collecto.GridBoard;
+import Collecto.*;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TUITest {
-
-
-//    public static void main(String[] args) {
-//        GridBoard board = new GridBoard();
-//        GridBoard copy = board.deepCopy();
-//
-////        board.moveLine(4, 4, GridBoard.Direction.DOWN);
-////        System.out.println(board.isMoveValid(4,4, GridBoard.Direction.DOWN));
-//
-////        System.out.println(board.checkSurroundings(0, 0));
-////        System.out.println(board.possibleMoves());
-//
-//        System.out.println(toString(board));
-//        System.out.println(plainTextBoard(board));
-//        System.out.println(colouredBoard(board));
-//
-//        System.out.println("Is board the same: "+board.toString().equals(copy.toString()));
-//    }
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream origOut = System.out;
 
     @BeforeEach
-    void setUp() {
+    public void setUpStreams() {
+        System.setOut(new PrintStream(out));
     }
 
-    @Test
-    void logTime() {
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(origOut);
     }
 
     @Test
     void currentDateTime() {
+        assertTrue(TUI.currentDateTime().contains(":"));
+        assertTrue(TUI.currentDateTime().contains("."));
     }
 
     @Test
-    void plainTextBoard() {
-    }
-
-    @Test
-    void colouredBoard() {
-    }
-
-    @Test
-    void testToString() {
+    void logTime() {
+        assertTrue(TUI.logTime().contains(Colour.GREY));
     }
 
     @Test
     void print() {
+        TUI.print("test string");
+        assertEquals(out.toString(), "test string\n");
     }
 
     @Test
     void printError() {
+        String string = "test error string";
+        TUI.printError(string);
+        assertTrue(out.toString().contains("ERROR"));
+        assertTrue(out.toString().contains(string));
+    }
+
+    @Test
+    void logError() {
+        String string = "test error string";
+        assertTrue(TUI.logError(string).contains("ERROR"));
+        assertTrue(TUI.logError(string).contains(string));
     }
 
     @Test
     void log() {
+        String string = "test string";
+        String log = TUI.log(string);
+        assertTrue(log.contains(":"));
+        assertTrue(log.contains("."));
+        assertTrue(log.contains(string));
+    }
+
+    private String checkBasicNumBoard() {
+        GridBoard board = new GridBoard(Misc.sampleBoardArray());
+        String textBoard = TUI.textBoard(board);
+        assertTrue(textBoard.contains("1") &&
+                textBoard.contains("2") &&
+                textBoard.contains("3") &&
+                textBoard.contains("4") &&
+                textBoard.contains("5") &&
+                textBoard.contains("6") &&
+                textBoard.contains("7"));
+        assertTrue(textBoard.contains("-------+") && textBoard.contains("|"));
+        assertTrue(textBoard.contains("BLUE"));
+
+        return textBoard;
+    }
+
+    @Test
+    void textBoard() {
+        checkBasicNumBoard();
+    }
+
+    @Test
+    void colouredBoard() {
+        String textBoard = checkBasicNumBoard();
+        assertTrue(textBoard.contains(Colour.BLUE));
+        assertTrue(textBoard.contains(Colour.RED));
+        assertTrue(textBoard.contains(Colour.RESET));
+    }
+
+    @Test
+    void boardString() {
+        GridBoard board = new GridBoard(Misc.sampleBoardArray());
+        String textBoard = TUI.textBoard(board);
+        assertTrue(textBoard.contains("-------+") && textBoard.contains("|"));
+        assertTrue(textBoard.contains("BLUE"));
     }
 
     @Test
     void printHelpClient() {
+        TUI.printHelpClient();
+        assertTrue(out.toString().contains(Global.Protocol.Commands.LIST));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.HELP));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.MOVE));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.HINT));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.BOARD));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.QUEUE));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.LOGS));
+        assertTrue(out.toString().contains(Global.Protocol.Commands.DISCONNECT));
     }
 
     @Test
     void ballColours() {
+        ArrayList<Ball> balls = new ArrayList<>();
+
+        assertEquals("", TUI.ballColours(balls));
+
+        balls.add(Ball.BLUE);
+        balls.add(Ball.RED);
+        balls.add(Ball.RED);
+
+        String temp = TUI.ballColours(balls);
+        assertEquals(3, temp.length() - temp.replaceAll("\u25CF", "").length());
     }
 }
