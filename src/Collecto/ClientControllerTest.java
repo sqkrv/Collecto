16fc9a4ca6440d8d81a642f32ee1dbff2a85e3a4
Hein <h.h.l.huijskes@student.utwente.nl>
2021-01-27 12:15:36 +0100

    Improved ClientControllerTest

--- src/Collecto/ClientControllerTest.java
+++ src/Collecto/ClientControllerTest.java
@@ -1,79 +1,39 @@
 package Collecto;
 
-import Collecto.Client;
-import Collecto.ClientController;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import Collecto.Tests.SocketFactory;
+import org.junit.jupiter.api.*;
 
 import java.io.*;
-import java.net.*;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.net.Socket;
 
-import static Collecto.Global.DELIMITER;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class ClientControllerTest {
 
     private ClientController controller;
-    private String answer;
-    private String result;
-    Socket clientSocket;
-    Socket serverSocket;
+    private Client client;
+    private Socket clientSocket;
+    private BufferedReader in;
+    private static Socket[] sockets;
 
-    public class SocketFactory implements Runnable {
-
-        private Socket socket1 = null;
-        private Socket socket2 = null;
-        private ServerSocket serverSocket;
-        private int port;
-
-        public SocketFactory(int port) {
-            this.port = port;
-            setServerSocket();
-        }
-        private void setServerSocket() {
-            try {
-                serverSocket = new ServerSocket(port);
-            } catch (IOException ignored) {
-
-            }
-        }
-
-        @Override
-        public void run() {
-            while (socket1 == null) {
-                try {
-                    socket1 = new Socket(InetAddress.getByName("localhost"), port);
-                } catch (IOException ignored) {
-                }
-            }
-        }
-
-        public Socket[] newSockets() {
-            try {
-                Thread thread = new Thread(this);
-                thread.start();
-                socket2 = serverSocket.accept();
-                thread.join();
-            } catch (IOException | InterruptedException ignored) {
-            }
-            return new Socket[]{socket1, socket2};
-        }
+    @BeforeAll
+    static void setUp() {
+        sockets = new SocketFactory(6666).newSockets();
     }
-
-    SocketFactory factory = new SocketFactory(6666);
 
     @BeforeEach
-    void before() {
-        Socket[] sockets = factory.newSockets();
+    void before() throws IOException {
         clientSocket = sockets[0];
-        serverSocket = sockets[1];
+        Socket serverSocket = sockets[1];
+        client = new Client(clientSocket);
+//        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(serverSocket.getOutputStream()));
+        in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
     }
 
-    void setUpReader(BufferedReader reader, Client client) {
+    void setUpController(String input) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
         this.controller = new ClientController(client) {
             @Override
             public void setScanner() {
@@ -88,11 +48,162 @@ class ClientControllerTest {
     }
 
     @Test
-    void start() throws IOException {
-        Client client = new Client(clientSocket);
-        BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-        setUpReader(new BufferedReader(new InputStreamReader(new ByteArrayInputStream("LIST\n".getBytes()))), client);
+    void start() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream("input".getBytes())));
+        controller = new ClientController(client) {
+            @Override
+            public void setScanner() {
+                this.scanner = new Scanner(reader);
+            }
+        };
+        assertThrows(NoSuchElementException.class, () -> controller.start());
+    }
+
+    @Test
+    void startOverride() throws IOException {
+        setUpController("LIST");
         controller.start();
         assertEquals(in.readLine(), "LIST");
+    }
+
+    @Test
+    void startOverrideWrong() {
+        setUpController("WRONG COMMAND");
+        controller.start();
+    }
+
+    @Nested()
+    @DisplayName("handleMove tests")
+    class handleMoveTests {
+
+        @Test
+        void helpTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void printHelp() {
+                    sendMessage("Helped");
+                }
+            };
+            setUpController("HELP");
+            controller.start();
+            assertEquals(in.readLine(), "Helped");
+        }
+
+        @Test
+        void queueTestInGame() {
+            client = new Client(clientSocket) {
+                @Override
+                public void handleNewGame(String[] params) {
+                    game = new Game("","");
+                }
+            };
+            client.handleNewGame(null);
+            setUpController("QUEUE");
+            controller.start();
+        }
+
+        @Test
+        void queueTestNotInGame() throws IOException {
+            setUpController("QUEUE");
+            controller.start();
+            assertEquals(in.readLine(), "QUEUE");
+        }
+
+        @Test
+        void listTest() throws IOException {
+            setUpController("LIST");
+            controller.start();
+            assertEquals(in.readLine(), "LIST");
+        }
+
+        @Test
+        void logsTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void printLogs() {
+                    sendMessage("Logged");
+                }
+            };
+            setUpController("LOGS");
+            controller.start();
+            assertEquals(in.readLine(), "Logged");
+        }
+
+        @Test
+        void boardTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void printBoard() {
+                    sendMessage("Board printed");
+                }
+            };
+            setUpController("BOARD");
+            controller.start();
+            assertEquals(in.readLine(), "Board printed");
+        }
+
+        @Test
+        void statusTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void status() {
+                    sendMessage("Status printed");
+                }
+            };
+            setUpController("STATUS");
+            controller.start();
+            assertEquals(in.readLine(), "Status printed");
+        }
+
+        @Test
+        void disconnectTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void disconnect() {
+                    sendMessage("Disconnected");
+                }
+            };
+            setUpController("DISCONNECT");
+            controller.start();
+            assertEquals(in.readLine(), "Disconnected");
+        }
+
+        @Test
+        void hintTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void hint() {
+                    sendMessage("Hinted");
+
+                    }
+                @Override
+                public void handleNewGame(String[] params) {
+                    game = new Game("","");
+                }
+            };
+            client.handleNewGame(null);
+            setUpController("HINT");
+            controller.start();
+            assertEquals(in.readLine(), "Hinted");
+        }
+
+        @Test
+        void moveTest() throws IOException {
+            client = new Client(clientSocket) {
+                @Override
+                public void handleMove(String[] params) {
+                    sendMessage("Moved");
+
+                }
+                @Override
+                public void handleNewGame(String[] params) {
+                    game = new Game("","");
+                }
+            };
+            client.handleNewGame(null);
+            setUpController("MOVE");
+            controller.start();
+            assertEquals(in.readLine(), "Moved");
+        }
     }
 }
